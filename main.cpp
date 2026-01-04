a02b160e84fe7fd0e171693bc32587604c5e1583
shuopeng.wu <1927059617@qq.com>
2026-01-04 06:49:38 +0000

    Update to library structure.

--- main.cpp
+++ main.cpp
@@ -1,23 +1,28 @@
-#include "log.h"
+#include "splog.h"
 #include <iostream>
+#include <string>
 
 namespace app {
 class A{
 public:
     void showLog(std::string s)
     {
-        logger.add_log(s);
+        splog::logger.add_log(s);
     }
 };
 }
 
 int main()
 {
+    {
+        splog::logger.set_sync_model(splog::LogSyncMode::Sync);
+        splog::logger.set_output_target(splog::LogOutputTarget::All);
+        splog::logger.set_filename("1.log");
+        splog::logger.set_flush_threshold(10000000);
+    }
     app::A a;
     std::string s;
-    while (true)
-    {
-        std::cin >> s;
-        a.showLog(s);
-    }
+
+    for (int i = 0; i < 1000; i++)
+        a.showLog(std::to_string(i));
 }
--- splog_p.cpp
+++ splog_p.cpp
@@ -1,4 +1,4 @@
-#include "log.h"
+#include "splog_p.h"
 #include <chrono>
 #include <iostream>
 
@@ -253,79 +253,6 @@ void splog::LogServer::full_handler()
 
         buffer.erase(0, i + 1);
     }
-}
-
-splog::Log &splog::Log::instance()
-{
-    static splog::Log log;
-    return log;
-}
-
-void splog::Log::set_sync_model(LogSyncMode mode)
-{
-    if (mode == LogSyncMode::Sync)
-        server = std::make_unique<SyncLogServer>();
-    else
-        server = std::make_unique<AsyncLogServer>();
-
-    client->set_handler([this](std::string data) {
-        server->sink(std::move(data));
-    });
-}
-
-void splog::Log::set_output_target(LogOutputTarget target)
-{
-    this->target = target;
-    client->set_target(target);
-}
-
-void splog::Log::set_filename(std::string_view filename)
-{
-    if (target & LogOutputTarget::File)
-    {
-        if (server)
-            server->set_file_name(filename);
-        else
-            std::cerr << "Please set the mode." << std::endl;
-    }
-    else
-    {
-        std::cerr << "Please enable file output before filename settings." << std::endl;
-    }
-}
-
-void splog::Log::set_flush_threshold(int flush_threshold)
-{
-    if (server)
-        server->set_flush_threshold(flush_threshold);
-    else
-        std::cerr << "Please set the mode." << std::endl;
-}
-
-void splog::Log::add_log(std::string_view log, LogLevel l, const std::source_location &location)
-{
-    client->add_log(log, l, location);
-    if (target & LogOutputTarget::File)
-    {
-        if (!server)
-            std::cerr << "Please set the mode." << std::endl;
-    }
-}
-
-void splog::Log::add_log(LogFormater log, LogLevel l, const std::source_location &location)
-{
-    client->add_log(log, l, location);
-    if (target & LogOutputTarget::File)
-    {
-        if (!server)
-            std::cerr << "Please set the mode." << std::endl;
-
-    }
-}
-
-splog::Log::Log()
-{
-    client = std::make_unique<LogClient>();
 }
 
 splog::SyncLogServer::~SyncLogServer()
